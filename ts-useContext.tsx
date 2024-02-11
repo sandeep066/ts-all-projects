a94f820c853b5d060e1038991fc9858ba9a94df0
sandeep066 <12527032+sandeep066@users.noreply.github.com>
2024-02-11 11:25:22 +0530

    Update word-wrap ts-useContext.tsx

--- ts-useContext.tsx
+++ ts-useContext.tsx
@@ -33,39 +33,51 @@ export { Parent, Child };
 /*
 1)const Store = createContext<IContext | undefined>(undefined);
 
-The reason for defining undefined twice in the createContext function call is to provide
-both the initial value and the default value for the context.
+The reason for defining undefined twice in the createContext 
+function call is to provide both the initial value and the default
+value for the context.
 
 Here's the breakdown:
 
 createContext<IContext | undefined>(undefined):
 
 This part of the code defines a new context called Store.
-IContext | undefined specifies the type of the context value. It means the context value can either be of type IContext or undefined.
-The undefined inside the angle brackets <...> provides the default value for the context. It specifies that the initial value of the
-context is undefined.
-This default value is used when a component consumes the context but there is no corresponding provider higher up in the component tree.
+IContext | undefined specifies the type of the context value. It means the
+context value can either be of type IContext or undefined.The undefined 
+inside the angle brackets <...>  provides the default value for the context. 
+It specifies that the initial value of the context is undefined.
+This default value is used when a component consumes the context but there is no
+corresponding provider higher up in the component tree.
+
 const Store = createContext<IContext | undefined>(undefined);:
 
-This line of code declares a constant variable named Store and initializes it with the created context using createContext.
-The context is defined with a type that allows either IContext or undefined.
-The initial value of the context is set to undefined as specified in the createContext call.
-By providing undefined as both the type and the initial value, we ensure that the context is correctly initialized 
-with an undefined value when no provider is present, and it's clear to TypeScript that the context value can potentially
-be undefined. This helps maintain type safety throughout the application when using the context.
+This line of code declares a constant variable named Store and 
+initializes it with the created context using createContext.
+The context is defined with a type that allows either IContext
+or undefined.
+The initial value of the context is set to undefined as specified in the 
+createContext call.
+By providing undefined as both the type and the initial value, we ensure 
+that the context is correctly initialized with an undefined value when no provider is
+present, and it's clear to TypeScript that the context value can potentially
+be undefined. This helps maintain type safety throughout the application 
+when using the context.
 
 2)const Parent: React.FC<ParentProps> = ({ children }) => {}
 
-React.FC stands for "React Function Component". It denotes that Parent is a function component in React.
+React.FC stands for "React Function Component". It denotes that 
+Parent is a function component in React.
 
 In TypeScript, React.FC is a generic type provided by the React library.
 It's used to define function components in React applications.
 
-The type parameter <ParentProps> specifies the type of props that the component expects to receive.
-In the context of React.FC<ParentProps>, ParentProps represents the input props for the component, i.e.,
+The type parameter <ParentProps> specifies the type of props
+that the component expects to receive.In the context of React.FC<ParentProps>,
+ParentProps represents the input props for the component, i.e.,
 the props that are passed into the component from its parent.
-So, in summary, React.FC is used to define a function component in React, and ParentProps represents 
-the input props for the Parent component.
+So, in summary, React.FC is used to define a function component
+in React, and ParentProps represents  the input props for the
+Parent component.
 
 
 */
